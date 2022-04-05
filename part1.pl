0cf564105d1327f56d6a8a544122471250824fb5
Marcus Oates <moates695@gmail.com>
2022-04-05 12:35:58 +1000

    sam must want coffee for robot to pick it up

--- part1.pl
+++ part1.pl
@@ -63,16 +63,16 @@ action(mcc,                                 % Move cc from off to cs
        state(cs, RHC, SWC, MW, RHM)).     
 
 action(puc,                                 % Pick up coffee                           
-       state(cs, false, SWC, MW, RHM),      % Before action, robot at Coffee Shop 
-                                            % without coffee
-       state(cs, true, SWC, MW, RHM)).      % After action, robot at Coffee Shop 
-                                            % with coffee
+       state(cs, false, true, MW, RHM),      % Before action, robot at Coffee Shop 
+                                            % without coffee and Sam wants coffee
+       state(cs, true, true, MW, RHM)).      % After action, robot at Coffee Shop 
+                                            % with coffee and Sam wants coffee
 
 action(dc,                                  % Deliver coffee
        state(off, true, _, MW, RHM),        % Before action, robot at Sams Office
                                             % with coffee
        state(off, false, false, MW, RHM)).  % After action, robot at Sams Office
-                                            % without coffee and Sam does not want coffee
+                                            % without coffee and Sam doesnt want coffee
 
 action(pum,                                 % Pick up mail
        state(mr, RHC, SWC, true, false),    % Before action, robot at Mail Room
